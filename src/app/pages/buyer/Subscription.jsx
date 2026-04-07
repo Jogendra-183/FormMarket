2d80365b2b65984c581e178a866659e85b0b9459
Jogendra-183 <2400030645@klniversity.in>
2026-04-07 22:55:42 +0530

    first commit

--- src/app/pages/buyer/Subscription.jsx
+++ src/app/pages/buyer/Subscription.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../components/ui/card";
